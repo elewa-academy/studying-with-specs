34e4f4975a7c12d14bcb013876269584653dab74
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-01-26 14:43:36 +0100

    demo ready

--- 0-how-to-use-these/4-reconstructed.js
+++
@@ -1,4 +0,0 @@
-/*
-	implement the behavior indicated in your empty code using whatever techniques you know, without referencing back to the study code
-	ie: if you don't remember how 'map' worked, make up another way to pass the arguments into the operation
-*/
--- game0/tictactoe0.js
+++
@@ -1,7 +0,0 @@
-//game 0 will
-// 		be a board you can write a thing on
-//  	check out how you can pass the board's context
-
-function play(box) {
-	box.innerHTML = box.id;
-}
