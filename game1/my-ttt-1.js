62a4c7f8971be4a436310930001422a2e47e29b8
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-04-20 10:10:57 +0200

    initial, empty behavior

---
+++ game1/my-ttt-1.js
@@ -0,0 +1,9 @@
+let nextPlayer = true;
+
+function play(box) {
+  // takes the box, and writes an x or o into the box
+  //      depending on who nextPlayer is
+  //      toggle nextPlayer
+      
+  return undefined
+}
